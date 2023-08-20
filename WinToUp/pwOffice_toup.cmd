0e90887cb0130465900325d8738df3c90d3c0c95
rnvbaldor <sdimetrecu@gmail.com>
2023-08-20 10:47:22 -0400

    add wintoup files repo::

---
+++ WinToUp/pwOffice_toup.cmd
@@ -0,0 +1,19 @@
+
+@echo off
+echo ###########
+echo # RUNNER  #
+echo ###########
+echo ...
+timeout /t  1 /nobreak
+echo comprobando...
+
+if exist "C:\Program Files\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files\Microsoft Office\Office16"
+
+if exist "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files (x86)\Microsoft Office\Office16"
+
+for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:kms.msgang.com
+cscript ospp.vbs /act
+echo Fin...
+pause
